a8c2dd6fa68c3b2f05e684661964b5d35ae7ceee
Chris Gannon <cjgannon@gmail.com>
2014-01-27 11:12:40 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Beef::Application.config.secret_key_base = '274d9bedfafb0bdfdea3e5853bd0bf8eecd579df5f2ce06f3b3f3cc7a649694bfa7af83272aef37f48c2a282be4a36a7aba8d1a992032fac60362b857765b4da'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Beef::Application.config.session_store :cookie_store, key: '_beef_session'
